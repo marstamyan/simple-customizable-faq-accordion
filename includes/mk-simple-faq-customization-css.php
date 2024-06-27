f6d685e933613fa4a4680343d0af55215127b8e8
Mamikon <marstamyan@mail.ru>
2024-06-27 19:53:40 +0400

    style fix

--- includes/mk-simple-faq-customization-css.php
+++ includes/mk-simple-faq-customization-css.php
@@ -18,7 +18,7 @@ $mk_faq_block_size = get_theme_mod( 'mk_faq_block_size', '320' );
 .mk-faq-accordion a.mk-faq-accordion-title:focus,
 .mk-faq-accordionTitleActive, .mk-faq-accordionTitle.is-expanded,
 .mk-faq-accordion-title {
-    background-color: <?php echo $title_background_color; ?>;
+    background-color: <?php echo $title_background_color; ?> !important;
 }
 
 .mk-faq-accordion-title:hover {
